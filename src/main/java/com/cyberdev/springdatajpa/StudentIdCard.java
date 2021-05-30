b3a20cf619e7c94b95edd79998500b9e81907564
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-05-30 23:23:28 +0300

    @OneToOne and @JoinColumn

--- src/main/java/com/cyberdev/springdatajpa/StudentIdCard.java
+++ src/main/java/com/cyberdev/springdatajpa/StudentIdCard.java
@@ -1,10 +1,13 @@
 package com.cyberdev.springdatajpa;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -44,6 +47,13 @@ public class StudentIdCard
           length = 15
   )
   private String cardNumber;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(
+          name = "student_id",
+          referencedColumnName = "id"
+  )
+  private Student student;
 
   public StudentIdCard() {
   }
