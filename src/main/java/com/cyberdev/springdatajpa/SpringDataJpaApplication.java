8b2a8882c39cf030f52f335a3dd2c47de311d5da
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-05-30 22:21:58 +0300

    Sorting

--- src/main/java/com/cyberdev/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/cyberdev/springdatajpa/SpringDataJpaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class SpringDataJpaApplication
@@ -19,19 +20,31 @@ public class SpringDataJpaApplication
   CommandLineRunner commandLineRunner(StudentRepository studentRepository)
   {
     return args -> {
-      Faker faker = new Faker();
-      for (int i = 0; i < 20; i++)
-      {
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-        String email = String.format("%s.%s@cyberdev.com", firstName, lastName);
-        int age = faker.number().numberBetween(17, 55);
+      generateRandomStudents(studentRepository);
 
-        Student student = new Student(firstName, lastName, email, age);
 
-        studentRepository.save(student);
-      }
+      Sort sort = Sort.by("firstName").ascending()
+              .and(Sort.by("age").descending());
+
+
+      studentRepository.findAll(sort)
+              .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
     };
+  }
+
+  private void generateRandomStudents(StudentRepository studentRepository) {
+    Faker faker = new Faker();
+    for (int i = 0; i < 20; i++)
+    {
+      String firstName = faker.name().firstName();
+      String lastName = faker.name().lastName();
+      String email = String.format("%s.%s@cyberdev.com", firstName, lastName);
+      int age = faker.number().numberBetween(17, 55);
+
+      Student student = new Student(firstName, lastName, email, age);
+
+      studentRepository.save(student);
+    }
   }
 
 }
