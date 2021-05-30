a0ef0f95b18ba783d3d9ef2e9c8c1b79295d827c
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-05-30 17:52:38 +0300

    Use some of the repository methods to retrieve, count or delete data

--- src/main/java/com/cyberdev/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/cyberdev/springdatajpa/SpringDataJpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringDataJpaApplication
 {
@@ -22,7 +24,40 @@ public class SpringDataJpaApplication
                                   "Ionescu",
                                   "ioana.ionescu@cyberit.ro",
                                   27);
-      studentRepository.save(ioana);
+
+      Student raul = new Student("Raul",
+              "Ciurescu",
+              "raul.ciurescu@cyberit.ro",
+              37);
+
+      System.out.println("Adding ioana and raul");
+      studentRepository.saveAll(List.of(ioana, raul));
+
+      System.out.print("Number of students");
+      System.out.println(studentRepository.count());
+
+
+      studentRepository
+              .findById(2L)
+              .ifPresentOrElse(
+                      System.out::println, () ->
+        System.out.println("Student with ID 2 not found"));
+
+      studentRepository
+              .findById(3L)
+              .ifPresentOrElse(
+                      System.out::println, () ->
+                              System.out.println("Student with ID 3 not found"));
+
+      System.out.println("Select all students");
+      List<Student> students = studentRepository.findAll();
+      students.forEach(System.out::println);
+
+      System.out.println("Delete ioana");
+      studentRepository.deleteById(1L);
+
+      System.out.print("Number of students");
+      System.out.println(studentRepository.count());
     };
   }
 
