514c78ef4371c02ab94d43dbae3121ece28d7397
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-05-30 21:56:43 +0300

    Installing Faker

--- src/main/java/com/cyberdev/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/cyberdev/springdatajpa/SpringDataJpaApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class SpringDataJpaApplication
 {
@@ -20,44 +18,6 @@ public class SpringDataJpaApplication
   CommandLineRunner commandLineRunner(StudentRepository studentRepository)
   {
     return args -> {
-      Student ioana = new Student("Ioana",
-                                  "Ionescu",
-                                  "ioana.ionescu@cyberit.ro",
-                                  27);
-
-      Student raul = new Student("Raul",
-                                 "Ciurescu",
-                                 "raul.ciurescu@cyberit.ro",
-                                 37);
-
-      Student raul2 = new Student("Raul",
-                                  "Ciurescu",
-                                  "raul2.ciurescu@cyberit.ro",
-                                  25);
-
-      Student raul3 = new Student("Raul",
-                                  "Ciurescu",
-                                  "raul3.ciurescu@cyberit.ro",
-                                  17);
-
-      studentRepository.saveAll(List.of(ioana, raul, raul2, raul3));
-
-      studentRepository.findStudentsByEmail("ioana.ionescu@cyberit.ro")
-          .ifPresentOrElse(System.out::println,
-                           () -> System.out
-                               .println("Student with ioana.ionescu@cyberit.ro not found."));
-
-      studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual("Raul", 25)
-              .forEach(System.out::println);
-
-      studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqualNative("Raul", 25)
-          .forEach(System.out::println);
-
-      studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqualNamedParameters("Ioana", 25)
-              .forEach(System.out::println);
-
-      System.out.println("Deleting Raul 4");
-      System.out.println(studentRepository.deleteStudentById(4L));
 
     };
   }
